d10755900882498f436d5dc5679309dff3eb5316
raythurnvoid <53383860+raythurnvoid@users.noreply.github.com>
2025-06-04 01:57:00 +0100

    #16076

---
+++ src/routes/16076/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { flushSync } from 'svelte';
+
+	let flag: boolean = $state(true);
+	let boolElText = $state(true);
+
+	async function handleClick() {
+		flushSync(() => {
+			boolElText = !boolElText;
+		});
+
+		flag = !flag;
+	}
+</script>
+
+<button onclick={handleClick}>switch</button>
+
+<div>flag : {flag}</div>
+
+{#if !flag}
+	<div>flag : {boolElText}</div>
+{/if}
